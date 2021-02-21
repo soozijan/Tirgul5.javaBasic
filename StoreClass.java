f369bd715f1d1341349ba37842cefe3ebcbcb40b
soozijan <75090137+soozijan@users.noreply.github.com>
2021-02-21 20:42:50 +0200

    Create StoreClass.java

---
+++ StoreClass.java
@@ -0,0 +1,12 @@
+package com.company.painter;
+
+import lombok.ToString;
+
+@ToString
+public class Store {
+
+    public String name;
+    //public final String address;
+    private String m_owner_name;
+
+}
