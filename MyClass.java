01bae56d702b6b11755ee5c1ae985c26f4d0468a
soozijan <75090137+soozijan@users.noreply.github.com>
2021-02-21 20:47:50 +0200

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,10 @@
+package com.company.painter;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class MyClass {
+    private @Getter int numClass;
+    private @Setter String nameClass;
+
+}
