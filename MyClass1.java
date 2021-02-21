b2b0c4b77f7c1b2f9e02c712431a2f5ebe7efc26
soozijan <75090137+soozijan@users.noreply.github.com>
2021-02-21 20:48:42 +0200

    Create MyClass1.java

---
+++ MyClass1.java
@@ -0,0 +1,14 @@
+package com.company.painter;
+
+public class MyClass1 {
+    private int numClass;
+    private String nameClass;
+
+    public int getNumClass()   {
+        return numClass;
+    }
+
+    public String getNameClass()   {
+        return nameClass;
+    }
+}
