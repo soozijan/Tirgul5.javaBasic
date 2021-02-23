d92456bc1b518ac1f48766bc865e5248a71001a9
soozijan <75090137+soozijan@users.noreply.github.com>
2021-02-23 17:16:02 +0200

    Create Main.forest.java

---
+++ Main.forest.java
@@ -0,0 +1,16 @@
+package com.forest;
+
+
+import com.creatures.Animal;
+import com.creatures.Bear;
+import com.creatures.Monkey;
+import com.creatures.Tiger;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Bear bear = new Bear();
+        Monkey monkey = new Monkey();
+        Tiger tiger = new Tiger();
