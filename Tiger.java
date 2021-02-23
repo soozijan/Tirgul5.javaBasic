08ce23e3472cc6c579b864d0e9e98a3b11daa40f
soozijan <75090137+soozijan@users.noreply.github.com>
2021-02-23 17:15:10 +0200

    Create Tiger.java

---
+++ Tiger.java
@@ -0,0 +1,9 @@
+package com.creatures;
+
+public class Tiger extends Animal {
+    private String color_of_tiger;
+
+    public void print_color_of_tiger()   {
+        System.out.println(String.format("The color of Tiger is: %s", color_of_tiger));
+    }
+}
