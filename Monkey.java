9c43b03f79ed3416bc2206d7e208f6667b5e40e6
soozijan <75090137+soozijan@users.noreply.github.com>
2021-02-23 17:14:24 +0200

    Create Monkey.java

---
+++ Monkey.java
@@ -0,0 +1,11 @@
+package com.creatures;
+
+public class Monkey extends Animal {
+    private float length_climb_on_tree;
+
+    public void print_lentgh_climb_on_tree()   {
+        System.out.println(String.format("The lentgh that Monkey can climb is:  %f", length_climb_on_tree));
+    }
+
+
+}
